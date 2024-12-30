e95dabccd34ebb267276b612d68e565566d8341b
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2024-12-30 16:27:33 -0300

    Added numeric modifiers (they break saving)

--- app/renderer.js
+++ app/renderer.js
@@ -23,9 +23,58 @@ const saveButton = document.getElementById("save");
 const historyList = document.getElementById("history");
 const savedSection = document.getElementById("saved-section");
 let descriptions;
+const modifierInput = document.getElementById("modifier");
+let breakdownText = "";
+let modifierText;
+let modifierDescription;
 
 updateDiceCounter();
 
+function updateDiceCounter()
+    {
+        if(selectedDice.length===0)
+            diceCounterDiv.innerText = "No estás por tirar ningún dado.";
+        else 
+            {
+                const counts = selectedDice.reduce((acc, die) => 
+                {
+                    acc[die.id] = (acc[die.id] || 0) + 1;
+                    return acc;
+                }, {});
+        
+                descriptions = Object.entries(counts)
+                    .map(([id, count]) => 
+                    {
+                        const sides = diceValues.find(die => die.id === id).sides;
+                        return {id, count, sides};
+                    })
+                    .sort((a, b) => b.sides - a.sides)
+                    .map(({count, id}) =>
+                    {
+                        return `${count}d${id.slice(1)}`
+                    })
+                
+                const descriptionText = descriptions.join(", ").replace(/, ([^,]*)$/, ' y $1')
+    
+                modifierDescription = "";
+                if(modifierInput.value > 0)
+                    modifierDescription = `, sumando ${modifierInput.value}`;
+                if(modifierInput.value < 0)
+                    modifierDescription = `, restando ${modifierInput.value.slice(1)}`;
+    
+                diceCounterDiv.innerText = `Estás por tirar ${descriptionText}${modifierDescription}.`;
+            }
+        console.log(selectedDice);
+}
+
+modifierInput.addEventListener("input", (e) => 
+    {
+        modifierInput.value = modifierInput.value.replace(/[^*/\-+0-9]/g, '');
+        updateDiceCounter();
+        if(e.target.value.indexOf("++") !== -1 || e.target.value.indexOf("--") !== -1)
+            e.target.value = e.target.value.slice(0, -1);
+    });
+    
 diceButtons.forEach(button => 
     {
         button.addEventListener("click", (e) => selectDice(e));
@@ -36,57 +85,16 @@ diceButtons.forEach(button =>
             });
     });
 
-function updateDiceCounter()
+function selectDice(event)
 {
-    if(selectedDice.length===0)
-        diceCounterDiv.innerText = "No estás por tirar ningún dado.";
-    else 
-        {
-            const counts = selectedDice.reduce((acc, die) => 
-            {
-                acc[die.id] = (acc[die.id] || 0) + 1;
-                return acc;
-            }, {});
-    
-            descriptions = Object.entries(counts)
-                .map(([id, count]) => 
-                {
-                    const sides = diceValues.find(die => die.id === id).sides;
-                    return {id, count, sides};
-                })
-                .sort((a, b) => b.sides - a.sides)
-                .map(({count, id}) =>
-                {
-                    return `${count}d${id.slice(1)}`
-                })
-            
-            diceCounterDiv.innerText = `Estás por tirar ${descriptions.join(", ").replace(/, ([^,]*)$/, ' y $1')}.`;
-        }
-    console.log(selectedDice);
-}
+    selectedDice.push(diceValues.find(die => die.id === event.target.id));
+    console.log(event.target.id);
+    if(event.target.getAttribute("data-modifier") !== undefined)
+    {
+        modifierInput.value = event.target.getAttribute("data-modifier");
+    }
 
-function calculateRolls()
-{
-    const diceSizes = selectedDice.map(die => 
-        diceValues.find(newDie => newDie.id === die.id).sides);
-    for(let i = 0; i < diceSizes.length; i++)
-        results.push(Math.floor(Math.random() * diceSizes[i]) + 1);
-    total = results.reduce((acc, result) => acc + result);
-
-    resultBreakdownDiv.innerText = results.join(" + "); 
-    resultDiv.innerText = total;
-}
-
-resetButton.addEventListener("click", resetSelected);
-function resetSelected()
-{
-    selectedDice.length = 0;
     updateDiceCounter();
-}
-
-function resetResults()
-{
-    results.length = 0;
 }
 
 rollButton.addEventListener("click", () => 
@@ -98,25 +106,80 @@ rollButton.addEventListener("click", () =>
         }
     });
 
-function addToHistory()
+function calculateRolls()
 {
-    if(results.length !== 0)
-    {
-        const newEntry = document.createElement("li");
-        historyList.insertBefore(newEntry, historyList.firstChild);
-        newEntry.innerText = `${total} (${results.join(" + ")})`
-        resetResults();
-        if(historyList.querySelectorAll("li").length > 10)
-        {
-            historyList.removeChild(historyList.lastChild);
-        }
-    }
+    const diceSizes = selectedDice.map(die => 
+        diceValues.find(newDie => newDie.id === die.id).sides);
+    for(let i = 0; i < diceSizes.length; i++)
+        results.push(Math.floor(Math.random() * diceSizes[i]) + 1);
+    total = (results.reduce((acc, result) => acc + result) + Number(modifierInput.value));
+
+    if (Number(modifierInput.value) > 0)
+        modifierText = ` + ${modifierInput.value}`;
+    else if (Number(modifierInput.value) < 0)
+        modifierText = ` - ${modifierInput.value.slice(1)}`;
+    else
+    modifierText = "";  
+
+    breakdownText = results.join(" + ") + modifierText;
+    resultBreakdownDiv.innerText = breakdownText;
+    resultDiv.innerText = total;
 }
 
-function selectDice(event)
+saveButton.addEventListener("click", saveRoll)
+function saveRoll()
 {
-    selectedDice.push(diceValues.find(die => die.id === event.target.id));
+    const modifierValue = Number(modifierInput.value);
+
+    if (modifierValue > 0)
+        modifierText = ` + ${modifierValue}`;
+    else if (modifierValue < 0)
+        modifierText = ` - ${Math.abs(modifierValue)}`;
+    else
+    modifierText = "";
+    const rollData = JSON.stringify
+        ({
+            dice: selectedDice.map(die => die.id),
+            modifier: modifierValue
+        });
+    
+    const displayName = descriptions.join(" + ") + modifierText;
+
+    // savedSection.querySelectorAll("button").forEach((existingButton) => 
+    // {
+    //     if(existingButton.getAttribute("id") === diceRoll)
+    //         existingButton.remove();
+    // })
+    const newSavedRoll = document.createElement("button");
+    newSavedRoll.setAttribute("data-roll", rollData);
+    newSavedRoll.innerText = displayName;
+    newSavedRoll.addEventListener("click", (e) => selectDice(e));
+    newSavedRoll.addEventListener("contextmenu", (e) =>
+        {
+            removeSavedRoll(e);
+            e.preventDefault();
+        });
+    savedSection.appendChild(newSavedRoll);
+}
+
+//#region Secondary functions 
+
+function removeSavedRoll(event)
+{
+    event.target.remove();
+}
+
+resetButton.addEventListener("click", resetRoll);
+function resetRoll()
+{
+    selectedDice.length = 0;
+    modifierInput.value = "";
     updateDiceCounter();
+}
+
+function resetResults()
+{
+    results.length = 0;
 }
 
 function removeDice(event)
@@ -127,28 +190,19 @@ function removeDice(event)
     updateDiceCounter();
 }
 
-saveButton.addEventListener("click", saveRoll)
-function saveRoll()
-{
-    const newSavedRollId = descriptions.join(" + ");
-    savedSection.querySelectorAll("button").forEach((existingButton) => 
-    {
-        if(existingButton.getAttribute("id") === newSavedRollId)
-            existingButton.remove();
-    })
-    const newSavedRoll = document.createElement("button");
-    newSavedRoll.setAttribute("id", newSavedRollId)
-    newSavedRoll.innerText = newSavedRollId;
-    newSavedRoll.addEventListener("click", (e) => selectDice(e));
-    newSavedRoll.addEventListener("contextmenu", (e) =>
-        {
-            removeSavedRoll(e);
-            e.preventDefault();
-        });
-    savedSection.appendChild(newSavedRoll);
-}
 
-function removeSavedRoll(event)
+function addToHistory()
 {
-    event.target.remove();
+    if(results.length !== 0)
+    {
+        const newEntry = document.createElement("li");
+        historyList.insertBefore(newEntry, historyList.firstChild);
+        newEntry.innerText = `${total} (${breakdownText})`
+        resetResults();
+        if(historyList.querySelectorAll("li").length > 10)
+        {
+            historyList.removeChild(historyList.lastChild);
+        }
+    }
 }
+//#endregion
